2ee4d83f549e6416f7e01d1d72eac781e9f89a1b
Chris Jedd <cjedd2000@yahoo.com>
2022-02-11 12:40:11 -0500

    SSID Config Updates

--- main/ProjectConfig.h
+++ main/ProjectConfig.h
@@ -1,5 +1,7 @@
 #define MDNS_HOST_NAME "PoolPumpCtrl"
 #define WEB_MOUNT_POINT "/www"
 
-#define WIFI_SSID "CJNET2"
-#define WIFI_PASSWORD "R2@79DQ0$551PXYDQ78"
+#define WIFI_SSID "YourSSID"
+#define WIFI_PASSWORD "YourPassword"
+
+#define
