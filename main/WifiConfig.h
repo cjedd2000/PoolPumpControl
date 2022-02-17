cfef3330d169bbe042af41ac30262784b223aa43
Chris Jedd <73202403+cjedd2000@users.noreply.github.com>
2022-02-17 10:36:37 -0500

    Update WifiConfig.h
    Added comments

--- main/WifiConfig.h
+++ main/WifiConfig.h
@@ -1,2 +1,12 @@
+#pragma once
+/**
+ * @file WifiConfig.h
+ * 
+ * @brief 
+ * Contains WiFi connection info until this project and dynamically update it.
+ * This file is not intened to be recommited so that SSIDs and passwords are not
+ * commited to public repositories.
+ */
+
 #define WIFI_SSID "YourSSID"
 #define WIFI_PASSWORD "YourPassword"
